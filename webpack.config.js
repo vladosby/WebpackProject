a1848ab64d1952f6d4fee8645df2f8c90ee3fb53
vladosby <vladislavn.kievski@gmail.com>
2016-09-11 14:37:18 +0300

    Adding environment variable, set up it default value, and use it in our module and in config file. To use in our module we have to use plugin DefinePlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 'use strict';
 
-let webpack = require("webpack");
+const webpack = require("webpack");
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
     entry: "./home",
     output: {
         filename: "build.js",
         library: "home"
     },
-    watch: true,
+    watch: NODE_ENV == 'development',
     watchOptions: {
         aggregateTimeout: 100
     },
     plugins: [
-        new webpack.OldWatchingPlugin()
+        new webpack.OldWatchingPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
     ],
-    devtool:"source-map"
+    devtool: NODE_ENV == 'development' ? "source-map" : null
 };
--- welcome.js
+++ welcome.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function (message) {
+
+    if (NODE_ENV == 'development') {
+        console.log('file welcome');
+    }
+    
     alert(`Welcome ${message}`);
 };
