ec45346f79f2035bfc670823f72803683ece2524
vladosby <vladislavn.kievski@gmail.com>
2016-09-11 13:12:55 +0300

    Adding watch changes, but it doesn't work correctly with NewWatchingPlugin, so i configured OldWatchingPlugin. Try later to delete this plugin.

--- home.js
+++ home.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let welcome = require('./welcome');
-
 welcome("home");
 
 exports.welcome = welcome;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 'use strict';
 
+let webpack = require("webpack");
 module.exports = {
     entry: "./home",
     output: {
         filename: "build.js",
         library: "home"
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 100
     }
+    ,
+    plugins: [
+        new webpack.OldWatchingPlugin()
+    ]
 };
