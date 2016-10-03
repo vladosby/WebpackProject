97a0a3d65bf09bbe6a70c978e3670209dfc60159
vladosby <vladislavn.kievski@gmail.com>
2016-10-03 22:38:57 +0300

    Adding Uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -40,3 +40,15 @@ module.exports = {
         }]
     }
 };
+
+if(NODE_ENV == 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            drop_console: true,
+            unsafe: true
+        }
+    })
+    );
+}
